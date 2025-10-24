673b0a9f436f12c7a6701731ee75661e26dca298
varun-1728 <varunreddy1728@gmail.com>
2025-10-24 19:57:40 +0530

    smt

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://clone-server-z2i6.onrender.com",
         changeOrigin: true,
         secure: false,
       },
