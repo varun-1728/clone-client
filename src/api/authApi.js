e4f66151e89875a5cf5d58d75a334cf3b0bd0fe2
varun-1728 <varunreddy1728@gmail.com>
2025-10-25 07:19:07 +0530

    smt3

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://clone-server-uqfd.onrender.com/api",
   withCredentials: true,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://clone-server-uqfd.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   plugins: [react(), tailwindcss()],
 });
